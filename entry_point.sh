2bccb832238d52c20f9f2a94c367aca8e09c6d16
Nikola Kovacs <nikola.kovacs@gmail.com>
2016-09-23 17:23:46 +0200

    initial commit

---
+++ entry_point.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source /opt/bin/functions.sh
+
+export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
+
+function shutdown {
+  kill -s SIGTERM $NODE_PID
+  wait $NODE_PID
+}
+
+if [ ! -z "$SE_OPTS" ]; then
+  echo "appending selenium options: ${SE_OPTS}"
+fi
+
+# phantomjs will want to put its log file somewhere it can write to
+cd /var/log/selenium
+java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
+  ${SE_OPTS} &
+NODE_PID=$!
+
+trap shutdown SIGTERM SIGINT
+wait $NODE_PID
